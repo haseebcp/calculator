43c85c435e8a8475e22b5240b79ee5192b5cd72a
Haseeb CP <hasibcp@gmail.com>
2019-02-14 14:59:31 +0530

    build

---
+++ docs/precache-manifest.4942f8d4ab51a57c0cf96e383ccb8a10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c20b3e0b47d7920ddd2c",
+    "url": "/static/js/main.c20b3e0b.chunk.js"
+  },
+  {
+    "revision": "8e3d66973cbc2f637ce7",
+    "url": "/static/js/1.8e3d6697.chunk.js"
+  },
+  {
+    "revision": "c20b3e0b47d7920ddd2c",
+    "url": "/static/css/main.df285104.chunk.css"
+  },
+  {
+    "revision": "8e3d66973cbc2f637ce7",
+    "url": "/static/css/1.244e904b.chunk.css"
+  },
+  {
+    "revision": "06ce87df82095686b4e69a4094001d34",
+    "url": "/index.html"
+  }
+];
