cc1914d8cf7ab2d66cb3515e87b8cb3b5723e3cc
Haseeb CP <hasibcp@gmail.com>
2019-02-14 16:03:21 +0530

    Updates

---
+++ precache-manifest.cee6bba2bbc27123651db2c69e75405d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "c44afc63c6c7f9d20c18",
+    "url": "/calculator/static/js/main.c44afc63.chunk.js"
+  },
+  {
+    "revision": "7f0da2e4962448d2cf07",
+    "url": "/calculator/static/js/1.7f0da2e4.chunk.js"
+  },
+  {
+    "revision": "c44afc63c6c7f9d20c18",
+    "url": "/calculator/static/css/main.df285104.chunk.css"
+  },
+  {
+    "revision": "7f0da2e4962448d2cf07",
+    "url": "/calculator/static/css/1.63611ed9.chunk.css"
+  },
+  {
+    "revision": "a156bce6648a4fcf0ecaf86446c0ecdf",
+    "url": "/calculator/index.html"
+  }
+];
